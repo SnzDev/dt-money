99a8a1d190066d68970855d2ecfa25ea01161ea2
SnzDev <tms.working@gmail.com>
2022-06-06 21:40:55 -0300

    Desenvolvido modal para criar nova transação

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,27 @@
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { useState } from 'react';
+import { NewTransactionModal } from './components/NewTransactionModal';
 
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModal] = useState(false);
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModal(true);
+  }
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModal(false);
+  }
+
   return (
     <>
       <GlobalStyle />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal} />
     </>
   );
 }
